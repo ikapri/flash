05ebde31813d8d05f319a5b13909718b99c97522
Ishaan Kapri <ishaankapri11@gmail.com>
2014-03-02 12:04:08 +0530

    words added

---
+++ flash/words.py
@@ -0,0 +1,447 @@
+WORDS = [
+    {
+        "definition": "to chuckle gleefully", 
+        "id": 1, 
+        "word": "chortle"
+    }, 
+    {
+        "definition": "an active hostility or opposition, as between unfriendly or conflicting groups:", 
+        "id": 2, 
+        "word": "antagonismn"
+    }, 
+    {
+        "definition": "full of menacing or malign influences; pernicious", 
+        "id": 3, 
+        "word": "baleful"
+    }, 
+    {
+        "definition": "to welcome or salute with shouts or sounds of joy and approval; applaud:", 
+        "id": 4, 
+        "word": "acclaim"
+    }, 
+    {
+        "definition": "marked by the characteristics of an earlier period; antiquated:", 
+        "id": 5, 
+        "word": "archaic"
+    }, 
+    {
+        "definition": "sour or astringent in taste:", 
+        "id": 6, 
+        "word": "acerbic"
+    }, 
+    {
+        "definition": "to find out definitely; learn with certainty or assurance; determine:", 
+        "id": 7, 
+        "word": "ascertain"
+    }, 
+    {
+        "definition": "a self-evident truth that requires no proof", 
+        "id": 8, 
+        "word": "axiomn"
+    }, 
+    {
+        "definition": "to examine or analyze:", 
+        "id": 9, 
+        "word": "assay"
+    }, 
+    {
+        "definition": "a noisy or drunken feast or social gathering; revelry", 
+        "id": 10, 
+        "word": "carousaln"
+    }, 
+    {
+        "definition": "a person or thing detested or loathed:", 
+        "id": 11, 
+        "word": "anatheman"
+    }, 
+    {
+        "definition": "subordinate; subsidiary", 
+        "id": 12, 
+        "word": "ancillary"
+    }, 
+    {
+        "definition": "with a turn or twist to one side; askew:", 
+        "id": 13, 
+        "word": "awry"
+    }, 
+    {
+        "definition": "to go before, in time, order, rank, etc.; precede:", 
+        "id": 14, 
+        "word": "antecede"
+    }, 
+    {
+        "definition": "to renounce, repudiate, or retract, esp with formal solemnity; recant:", 
+        "id": 15, 
+        "word": "abjure"
+    }, 
+    {
+        "definition": "to refuse or deny oneself (some rights, conveniences, etc.); reject; renounce", 
+        "id": 16, 
+        "word": "abnegate"
+    }, 
+    {
+        "definition": "to shorten by omissions while retaining the basic contents:", 
+        "id": 17, 
+        "word": "abridge"
+    }, 
+    {
+        "definition": "a salt of hydrobromic acid consisting of two elements, one of which is bromine, as sodium bromide, NaBr", 
+        "id": 18, 
+        "word": "bromiden"
+    }, 
+    {
+        "definition": "harsh discordance of sound; dissonance:", 
+        "id": 19, 
+        "word": "cacophonyn"
+    }, 
+    {
+        "definition": "to caution, ise, or counsel against something", 
+        "id": 20, 
+        "word": "admonish"
+    }, 
+    {
+        "definition": "temporary inactivity, cessation, or suspension:", 
+        "id": 21, 
+        "word": "abeyancen"
+    }, 
+    {
+        "definition": "a feeling of extreme repugnance or aversion; utter loathing; abomination", 
+        "id": 22, 
+        "word": "abhorrencen"
+    }, 
+    {
+        "definition": "devoid of freshness or originality; hackneyed; trite:", 
+        "id": 23, 
+        "word": "banal"
+    }, 
+    {
+        "definition": "to hold oneself back voluntarily, esp from something regarded as improper or unhealthy (usually fol by from):", 
+        "id": 24, 
+        "word": "abstain"
+    }, 
+    {
+        "definition": "pertaining to bile or to an excess secretion of bile", 
+        "id": 25, 
+        "word": "bilious"
+    }, 
+    {
+        "definition": "known or understood by very few; mysterious; secret; obscure; esoteric:", 
+        "id": 26, 
+        "word": "arcane"
+    }, 
+    {
+        "definition": "having, expressive of, or characterized by intense feeling; passionate; fervent:", 
+        "id": 27, 
+        "word": "ardent"
+    }, 
+    {
+        "definition": "a dead body, esp a human body to be dissected; corpse", 
+        "id": 28, 
+        "word": "cadavern"
+    }, 
+    {
+        "definition": "a festival in honor of Bacchus", 
+        "id": 29, 
+        "word": "bacchanaliann"
+    }, 
+    {
+        "definition": "a small object worn to ward off evil, harm, or illness or to bring good fortune; protecting charm", 
+        "id": 30, 
+        "word": "amuletn"
+    }, 
+    {
+        "definition": "something or someone that is not in its correct historical or chronological time, esp a thing or person that belongs to an earlier time:", 
+        "id": 31, 
+        "word": "anachronismn"
+    }, 
+    {
+        "definition": "favoring complete obedience or subjection to authority as opposed to individual freedom:", 
+        "id": 32, 
+        "word": "authoritarian"
+    }, 
+    {
+        "definition": "deviating from or inconsistent with the common order, form, or rule; irregular; abnormal:", 
+        "id": 33, 
+        "word": "anomalous"
+    }, 
+    {
+        "definition": "failing to succeed; unsuccessful:", 
+        "id": 34, 
+        "word": "abortive"
+    }, 
+    {
+        "definition": "to estimate the monetary value of; determine the worth of; assess:", 
+        "id": 35, 
+        "word": "appraise"
+    }, 
+    {
+        "definition": "a long, often cylindrical, cushion or pillow for a bed, sofa, etc", 
+        "id": 36, 
+        "word": "bolstern"
+    }, 
+    {
+        "definition": "a traditional saying expressing a common experience or observation; proverb", 
+        "id": 37, 
+        "word": "adagen"
+    }, 
+    {
+        "definition": "subject to, led by, or indicative of caprice or whim; erratic:", 
+        "id": 38, 
+        "word": "capricious"
+    }, 
+    {
+        "definition": "(of a vehicle) to lean, sway, or tip to one side while in motion:", 
+        "id": 39, 
+        "word": "careen"
+    }, 
+    {
+        "definition": "an opening, as a hole, slit, crack, gap, etc", 
+        "id": 40, 
+        "word": "aperturen"
+    }, 
+    {
+        "definition": "a feeling of vexation, marked by disappointment or humiliation", 
+        "id": 41, 
+        "word": "chagrinn"
+    }, 
+    {
+        "definition": "a shot in which the cue ball hits two balls in succession", 
+        "id": 42, 
+        "word": "caromn"
+    }, 
+    {
+        "definition": "to regard or portray as less impressive or important than appearances indicate; depreciate; disparage", 
+        "id": 43, 
+        "word": "belittle"
+    }, 
+    {
+        "definition": "conferring benefit; antageous; helpful:", 
+        "id": 44, 
+        "word": "beneficial"
+    }, 
+    {
+        "definition": "to bring to a state of peace, quiet, ease, calm, or contentment; pacify; soothe:", 
+        "id": 45, 
+        "word": "appease"
+    }, 
+    {
+        "definition": "(of speech, writing, etc.) high-sounding; high-flown; inflated; pretentious", 
+        "id": 46, 
+        "word": "bombastic"
+    }, 
+    {
+        "definition": "to relieve from a charge of fault or crime; declare not guilty:", 
+        "id": 47, 
+        "word": "acquit"
+    }, 
+    {
+        "definition": "unselfishly concerned for or devoted to the welfare of others (", 
+        "id": 48, 
+        "word": "altruistic"
+    }, 
+    {
+        "definition": "to make or become better, more bearable, or more satisfactory; improve; meliorate", 
+        "id": 49, 
+        "word": "ameliorate"
+    }, 
+    {
+        "definition": "promising success; propitious; opportune; favorable:", 
+        "id": 50, 
+        "word": "auspicious"
+    }, 
+    {
+        "definition": "to destroy the self-confidence, poise, or self-possession of; disconcert; make ashamed or embarrassed:", 
+        "id": 51, 
+        "word": "abash"
+    }, 
+    {
+        "definition": "departing from the right, normal, or usual course", 
+        "id": 52, 
+        "word": "aberrant"
+    }, 
+    {
+        "definition": "situated before or at the front of; fore (", 
+        "id": 53, 
+        "word": "anterior"
+    }, 
+    {
+        "definition": "a natural, basic, or habitual repugnance; aversion", 
+        "id": 54, 
+        "word": "antipathyn"
+    }, 
+    {
+        "definition": "of doubtful authorship or authenticity", 
+        "id": 55, 
+        "word": "apocryphal"
+    }, 
+    {
+        "definition": "to free from guilt or blame or their consequences:", 
+        "id": 56, 
+        "word": "absolve"
+    }, 
+    {
+        "definition": "to widen in scope; increase in size or intensity; enlarge; extend", 
+        "id": 57, 
+        "word": "aggrandize"
+    }, 
+    {
+        "definition": "one of a group of ancient Roman officials charged with observing and interpreting omens for guidance in public affairs", 
+        "id": 58, 
+        "word": "augurn"
+    }, 
+    {
+        "definition": "patronage; support; sponsorship:", 
+        "id": 59, 
+        "word": "auspicen"
+    }, 
+    {
+        "definition": "trickery or deception by quibbling or sophistry:", 
+        "id": 60, 
+        "word": "chicaneryn"
+    }, 
+    {
+        "definition": "the act of departing from the right, normal, or usual course", 
+        "id": 61, 
+        "word": "aberrationn"
+    }, 
+    {
+        "definition": "destructive; pernicious:", 
+        "id": 62, 
+        "word": "baneful"
+    }, 
+    {
+        "definition": "a terse saying embodying a general truth, or astute observation, as \"Power tends to corrupt, and absolute power corrupts absolutely\" (Lord Acton).", 
+        "id": 63, 
+        "word": "aphorismn"
+    }, 
+    {
+        "definition": "a short, pithy, instructive saying; a terse remark or aphorism", 
+        "id": 64, 
+        "word": "apothegmn"
+    }, 
+    {
+        "definition": "to deprive and make desolate, esp by death (usually fol by of):", 
+        "id": 65, 
+        "word": "bereave"
+    }, 
+    {
+        "definition": "doing good or causing good to be done; conferring benefits; kindly in action or purpose", 
+        "id": 66, 
+        "word": "beneficent"
+    }, 
+    {
+        "definition": "slightly sour", 
+        "id": 67, 
+        "word": "acidulous"
+    }, 
+    {
+        "definition": "open to or having several possible meanings or interpretations; equivocal:", 
+        "id": 68, 
+        "word": "ambiguous"
+    }, 
+    {
+        "definition": "extremely bold or daring; recklessly brave; fearless:", 
+        "id": 69, 
+        "word": "audacious"
+    }, 
+    {
+        "definition": "to encourage, support, or countenance by aid or approval, usually in wrongdoing:", 
+        "id": 70, 
+        "word": "abet"
+    }, 
+    {
+        "definition": "a false or baseless, usually derogatory story, report, or rumor", 
+        "id": 71, 
+        "word": "canardn"
+    }, 
+    {
+        "definition": "to refuse or deny oneself (some rights, conveniences, etc.); reject; renounce", 
+        "id": 72, 
+        "word": "abnegation"
+    }, 
+    {
+        "definition": "hard to understand; recondite; esoteric:", 
+        "id": 73, 
+        "word": "abstruse"
+    }, 
+    {
+        "definition": "a person who dedicates his or her life to a pursuit of contemplative ideals and practices extreme self-denial or self-mortification for religious reasons", 
+        "id": 74, 
+        "word": "asceticn"
+    }, 
+    {
+        "definition": "with suspicion, mistrust, or disapproval:", 
+        "id": 75, 
+        "word": "askance"
+    }, 
+    {
+        "definition": "to make milder or less severe; relieve; ease; mitigate:", 
+        "id": 76, 
+        "word": "assuage"
+    }, 
+    {
+        "definition": "of keen penetration or discernment; sagacious:", 
+        "id": 77, 
+        "word": "astute"
+    }, 
+    {
+        "definition": "of, pertaining to, or capable of walking:", 
+        "id": 78, 
+        "word": "ambulatory"
+    }, 
+    {
+        "definition": "to raise irritating and trivial objections; find fault with unnecessarily (usually fol by at or about):", 
+        "id": 79, 
+        "word": "cavil"
+    }, 
+    {
+        "definition": "like a churl; boorish; rude:", 
+        "id": 80, 
+        "word": "churlish"
+    }, 
+    {
+        "definition": "with speed; quickly; swiftly", 
+        "id": 81, 
+        "word": "apace"
+    }, 
+    {
+        "definition": "sudden or unexpected:", 
+        "id": 82, 
+        "word": "abrupt"
+    }, 
+    {
+        "definition": "any award, honor, or laudatory notice:", 
+        "id": 83, 
+        "word": "accoladen"
+    }, 
+    {
+        "definition": "uttered clearly in distinct syllables", 
+        "id": 84, 
+        "word": "articulate"
+    }, 
+    {
+        "definition": "caustic, stinging, or bitter in nature, speech, behavior, etc.:", 
+        "id": 85, 
+        "word": "acrimonious"
+    }, 
+    {
+        "definition": "a damaging or derogatory remark or criticism; slander:", 
+        "id": 86, 
+        "word": "aspersionn"
+    }, 
+    {
+        "definition": "careful; cautious; prudent:", 
+        "id": 87, 
+        "word": "canny"
+    }, 
+    {
+        "definition": "to speak or write in favor of; support or urge by argument; recommend publicly:", 
+        "id": 88, 
+        "word": "ocate"
+    }, 
+    {
+        "definition": "capable of burning, corroding, or destroying living tissue", 
+        "id": 89, 
+        "word": "caustic"
+    }
+]
