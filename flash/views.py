ac2efe24d5f7bc01baa1b1c480f3cb013ae2c92f
Ishaan Kapri <ishaankapri11@gmail.com>
2014-03-02 12:04:36 +0530

    view function added

---
+++ flash/views.py
@@ -0,0 +1,32 @@
+__author__ = 'ishaan'
+
+from flash import app
+from flask import render_template, session, request
+import random
+from words import WORDS
+import json
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        words = random.sample(WORDS, 10)
+        session['ids'] = [w['id']-1 for w in words]
+        print session['ids']
+        return render_template('index.html', data=json.dumps(words))
+    else:
+        if not session.get('ids'):
+            session['ids'] = []
+        if len(session['ids']) >= len(WORDS):
+            return json.dumps([])
+        ids = range(1, len(WORDS)+1)
+        for i in session['ids']:
+            ids.remove(i)
+        print ids
+        if len(ids) <= 10:
+            rand_ids = ids
+        else:
+            rand_ids = random.sample(ids, 10)
+        session['ids'] += rand_ids
+        data = [WORDS[i-1] for i in rand_ids]
+        return json.dumps(data)
